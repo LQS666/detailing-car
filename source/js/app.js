2213a4c2ed615422ca6651d3b5bfa2256c7c6546
Paweł Turkot <p.turkot123@gmail.com>
2019-01-13 18:01:00 +0100

    Add hero and nav

---
+++ source/js/app.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+    // Obsługa responsywnego menu typu hamburger
+    let menu = $(".nav-ham");
+    let hamb = $(".ham-icon");
+    let body = $("body");
+
+    hamb.click(function () {
+        menu.toggleClass("open");
+        body.toggleClass("menu_open");
+    });
+
+    $(window).resize(function () {
+        let curWidth = $(window).width();
+        console.log(curWidth);
+
+        if (curWidth >= 580) {
+            closeMenu();
+        }
+    });
+
+    $(".nav-ham li").click(function () {
+        closeMenu();
+    });
+
+    function closeMenu() {
+        body.removeClass("menu_open");
+        menu.removeClass("open");
+    }
+
+    // Efekt animowanej kreski" po najechaniu na elementy menu
+    $(window).on('load resize', function () {
+        let $thisnav = $('.current-menu-item').offset().left;
+
+        $('.main-nav-item').hover(function () {
+            let $left = $(this).offset().left - $thisnav;
+            let $width = $(this).outerWidth();
+            let $start = 0;
+            $('.menu-sel').css({
+                'left': $left,
+                'width': $width
+            });
+        }, function () {
+            let $initwidth = $('.current-menu-item').width();
+            $('.menu-sel').css({
+                'left': '0',
+                'width': $initwidth
+            });
+        });
+    });
+
+
+
+});
