f6a4a18e717d754ee97e45f45448fdc5221b4dc5
Paweł Turkot <p.turkot123@gmail.com>
2019-01-14 01:57:38 +0100

    Fix nav script

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-    // Obsługa responsywnego menu typu hamburger
+
+
+    // Hamburger menu
     let menu = $(".nav-ham");
     let hamb = $(".ham-icon");
     let body = $("body");
@@ -11,7 +13,6 @@ $(document).ready(function () {
 
     $(window).resize(function () {
         let curWidth = $(window).width();
-        console.log(curWidth);
 
         if (curWidth >= 580) {
             closeMenu();
@@ -27,30 +28,34 @@ $(document).ready(function () {
         menu.removeClass("open");
     }
 
-    // Efekt animowanej kreski" po najechaniu na elementy menu
-    $(window).on('load resize', function () {
-        let $thisnav = $('.current-menu-item').offset().left;
 
-        $('.main-nav-item').hover(function () {
-            let $left = $(this).offset().left - $thisnav;
-            let $width = $(this).outerWidth();
-            let $start = 0;
-            $('.menu-sel').css({
-                'left': $left,
-                'width': $width
-            });
-        }, function () {
-            let $initwidth = $('.current-menu-item').width();
-            $('.menu-sel').css({
-                'left': '0',
-                'width': $initwidth
-            });
+    // 1. No anchor in url adress
+    // 2. Smooth Scroll
+    // 3. Active Class
+    $('a[href*="#"]:not([href="#"])').click(function (event) {
+        event.preventDefault();
+        let a = 0;
+        if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+            a = $(document).height() - $(window).height();
+        } else {
+            a = $(this.hash).offset().top - 24;
+        }
+        $('html,body').stop().animate({
+            scrollTop: a
+        }, 600, 'swing');
+    });
+    $('.main-nav-item a').click(function () {
+        $(this).parent().find('a').removeClass('selected-a');
+        $(this).addClass('selected-a');
+    });
+    $(window).scroll(function () {
+        $('.section').each(function () {
+            if ($(window).scrollTop() >= $(this).offset().top - 50) {
+                let id = $(this).attr('id');
+                $('.main-nav-item a').removeClass('selected-a');
+                $('.main-nav-item a[href=#' + id + ']').addClass('selected-a');
+            }
         });
     });
-
-
-
-
-
 
 });
